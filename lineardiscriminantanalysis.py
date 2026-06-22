27e9984b10f88e90cea81e41957421ae3bf272c7
unknown <arminjafri138386@gmail.com>
2026-06-21 22:00:15 -0700

    lineardiscriminantananlysis

---
+++ lineardiscriminantanalysis.py
@@ -0,0 +1,9 @@
+import numpy as np 
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+X = np.array([[-1,-1],[-2,-1],[-3,-2],[1,1],[2,1],[3,2]])
+y = np.array([1,1,1,2,2,2])
+clf = LinearDiscriminantAnalysis()
+y1 = clf.fit(X,y)
+print(y1)
+
+print(clf.predict([[-0.8,-1]]))
