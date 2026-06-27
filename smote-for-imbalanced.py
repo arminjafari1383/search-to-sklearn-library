3bdd3e2d63d4c3ebcaf684c08727d20e89808ca5
unknown <arminjafri138386@gmail.com>
2026-06-26 22:50:06 -0700

    Train Logistic Regression After SMOTE

--- smote-for-imbalanced.py
+++ smote-for-imbalanced.py
@@ -33,9 +33,24 @@ X_train,X_test,y_train,y_test = train_test_split(
 )
 smote = SMOTE(random_state=42)
 X_train_sm, y_train_sm = smote.fit_resample(X_train, y_train)
-plt.bar(y_train_sm.value_counts().index,y_train_sm.value_counts().values,color=['skyblue','salmon'])
-plt.xticks([0,1],['Non-Fraud','Fraud'])
-plt.ylabel("Count")
-plt.title("Class Distribution AFTER SMOTE")
+
+# plt.bar(y_train_sm.value_counts().index,y_train_sm.value_counts().values,color=['skyblue','salmon'])
+# plt.xticks([0,1],['Non-Fraud','Fraud'])
+# plt.ylabel("Count")
+# plt.title("Class Distribution AFTER SMOTE")
+# plt.show()
+
+
+#### Train Logistic Regression After SMOTE
+model = LogisticRegression(random_state=42,max_iter=1000)
+model.fit(X_train_sm, y_train_sm)
+y_pred_sm = model.predict(X_test)
+print("Accuracy AFTER SMOTE:",round(accuracy_score(y_test,y_pred_sm)*100,2),"%\n")
+print("Classification Report AFTER SMOTE:\n",classification_report(y_test,y_pred_sm))
+
+sns.heatmap(confusion_matrix(y_test,y_pred_sm),annot=True,fmt='d',cmap='Blues')
+plt.title("Confusion Matrix AFTER SMOTE")
+plt.xlabel("Predicted")
+plt.ylabel("Actual")
 plt.show()
 
