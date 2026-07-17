2dd765304fd702e55ccfd048e23a4ec8089e5cda
unknown <arminjafri138386@gmail.com>
2026-07-17 00:10:19 -0700

    grid_to_graph

---
+++ featureextraction/extractpatches2d.py
@@ -0,0 +1,14 @@
+from sklearn.datasets import load_sample_image
+from sklearn.feature_extraction import image
+# use the array data from the first image in this dataset
+one_image = load_sample_image("china.jpg")
+# print('Image shape : {}'.format(one_image.shape))
+
+patches = image.extract_patches_2d(one_image,(2,2))
+# print('Patches shape: {}'.format(patches.shape))
+
+#Here are just two of these patches
+
+print(patches[1])
+
+print(patches[800])
